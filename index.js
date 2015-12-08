f7bb0a1573cac8bbd90d49d121063decd81a006a
@omglikesowhat <jeremy.reichardt@gmail.com>
2015-12-08 14:10:34 -0800

    Update index.js
    moved from regex pattern to simple string

--- index.js
+++ index.js
@@ -2,50 +2,48 @@ var fs = require('fs');
 var path = require('path');
 var through = require('through2');
 var xtend = require('xtend');
-var mime = require('mime');
 
 module.exports = function(options) {
-    var opts = xtend({
-        basepath: '',
-        pattern: /'@@import ([a-zA-Z0-9\-_.\\/]+)'/g,
-        debug: false
-    }, options);
+  var opts = xtend({
+    basepath: '',
+    pattern: 'index.js'
+  }, options);
 
-    opts.debug && console.log('Injecting resources:');
+  opts.debug && console.log('Injecting resources:');
 
-    return through.obj(
-        function(file, enc, callback) {
-            if (file.isNull()) callback(null, file);
+  return through.obj(
 
-            var process = function(contents) {
-                return contents.replace(opts.pattern, function(match, filepath) {
-                    var fp = path.join(opts.basepath, filepath);
+    function(file, enc, callback) {
 
-                    try {
-                        var filecontents = mime.lookup(filepath) + ';base64,' + fs.readFileSync(filepath).toString('base64');
-                        opts.debug && console.log('   ', filepath, 'OK');
-                        return "'" + filecontents + "'";
-                    }
-                    catch (e) {
-                        console.error('unable to read utf8 file', fp);
-                        return "''";
-                    }
-                });
-            };
+      if (file.isNull()) callback(null, file);
 
-            opts.debug && console.log(' ', file.path);
+      var process = function(contents) {
 
-            if (file.isBuffer()) {
-                file.contents = new Buffer(process(String(file.contents)));
-                callback(null, file);
-            } else if (file.isStream()) {
-                file.contents.on('data', function(data) {
-                    file.contents = new Buffer(process(data));
-                    callback(null, file);
-                });
-            } else { 
-                callback(null, file);
-            }
-        }
-    );
+        return contents.replace(opts.pattern, function(match, filepath) {
+
+          var fp = path.join(opts.basepath, opts.pattern);
+
+          try {
+            var filecontents = fs.readFileSync(fp, { encoding: 'base64' });
+            return 'data:text/javascript;base64, ' + filecontents;
+          }
+          catch (e) {
+            return "''";
+          }
+        });
+      };
+
+      if (file.isBuffer()) {
+        file.contents = new Buffer(process(String(file.contents)));
+        callback(null, file);
+      } else if (file.isStream()) {
+        file.contents.on('data', function(data) {
+          file.contents = new Buffer(process(data));
+          callback(null, file);
+        });
+      } else {
+        callback(null, file);
+      }
+    }
+  );
 };
